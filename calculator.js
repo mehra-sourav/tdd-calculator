4cf4ade24c3c931ba6dc16b08ffb60b314ac6302
Sourav <1997mehra@gmail.com>
2024-07-09 18:42:39 +0530

    feat: add test case and functionality for comma separated numbers
    - Added a new test case to check if a string with comma separated numbers returns their sum
    - Made the test case pass by implementing the functionality to handle comma separated numbers in the string calculator

--- calculator.js
+++ calculator.js
@@ -2,6 +2,11 @@ const add = (stringInput) => {
   if (stringInput === "") {
     return 0;
   }
+
+  let inputNumbers = stringInput.split(",");
+  let result = inputNumbers.reduce((sum, curr) => sum + Number(curr), 0);
+
+  return result;
 };
 
 module.exports = add;
--- calculator.test.js
+++ calculator.test.js
@@ -1,7 +1,17 @@
 const add = require("./calculator");
 
-test("Test runs", () => {
-  const inputsOutputs = [["", 0]];
+test("Evaluates empty string to 0", () => {
+  let result = add("");
+  expect(result).toBe(0);
+});
+
+test("Evaluates string with comma separated numbers", () => {
+  const inputsOutputs = [
+    ["2", 2],
+    ["2,3", 5],
+    ["2,3,4", 9],
+    ["2,3,4,5", 14],
+  ];
 
   inputsOutputs.forEach(([input, expectedOutput]) => {
     let result = add(input);
