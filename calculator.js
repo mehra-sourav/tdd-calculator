e0db997db5794dabf76a3458d513325607a0dee5
Sourav <1997mehra@gmail.com>
2024-07-27 16:31:49 +0530

    feat: add functionality for calculating difference of sum of odd and even numbers when delimiteri is '@'     - Added a new functionality to check if the difference of the sums of odd and even numbers is calculated when the delimiter is '@'

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 const getNumbersFromString = (string) => {
-  const delimiterPattern = /-?\d+/g;
-  return string.match(delimiterPattern)?.map((num) => Number(num)) ?? [];
+    const delimiterPattern = /-?\d+/g;
+    return string.match(delimiterPattern)?.map((num) => Number(num)) ?? [];
 };
 
 const add = (stringInput) => {
@@ -8,8 +8,12 @@ const add = (stringInput) => {
     return 0;
   }
 
-  let inputNumbers = getNumbersFromString(stringInput);
+  const delimiterPattern = /\/\/@/g;
+  const numberPattern = /-?\d+/g;
+  
+  let inputNumbers = stringInput.match(numberPattern)?.map((num) => Number(num)) ?? [];
   let negativeNumbers = inputNumbers.filter((num) => num < 0);
+  
 
   if (negativeNumbers.length > 0) {
     let errorMsg = negativeNumbers.reduce((msg, num) => `${msg}${num},`, "");
@@ -19,9 +23,28 @@ const add = (stringInput) => {
     )}`;
   }
 
-  let result = inputNumbers.reduce((sum, curr) => {
-    return sum + (curr > 1000 ? 0 : Number(curr));
-  }, 0);
+  let result;
+  
+  // If contains '@' as delimiter
+  if (stringInput.match(delimiterPattern)?.length > 0) {
+    let oddSum = 0, evenSum = 0;
+
+    inputNumbers.forEach((num, idx) => {
+      if (num % 2 === 0) {
+        evenSum += num;
+      }
+      else {
+        oddSum += num;
+      }
+    });
+
+    result = oddSum - evenSum;
+  }
+  else {
+      result = inputNumbers.reduce((sum, curr) => {
+        return sum + (curr > 1000 ? 0 : Number(curr));
+      }, 0);
+  }
 
   return result;
 };
